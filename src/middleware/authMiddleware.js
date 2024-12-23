97370971505442d1c7b4c0c03f296f8089f749fe
elizadoltu <elizadoltuofficial@gmail.com>
2024-12-23 14:31:29 +0200

    Refactor auth middleware to resolve public key path and handle missing file error

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,14 +1,23 @@
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
+import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const publicKey = fs.readFileSync(process.env.PUBLIC_KEY_PATH, 'utf8');
+// Resolve the full path of the public key using path.resolve
+const publicKeyPath = path.resolve(process.cwd(), process.env.PUBLIC_KEY_PATH);
+
+if (!fs.existsSync(publicKeyPath)) {
+    console.error('Public key file not found:', publicKeyPath);
+    process.exit(1); // Exit the application if the file is not found
+}
+
+const publicKey = fs.readFileSync(publicKeyPath, 'utf8');
 
 export const authMiddleware = (req, res, next) => {
     // Bearer token
-    const token = req.header('Authorization')?.split(' ')[1] 
+    const token = req.header('Authorization')?.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'Access denied. No token provided' });
 
     try {
