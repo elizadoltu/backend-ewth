5fbe5a9096e5d60db11eaf4b0f4cc00872b064c6
elizadoltu <elizadoltuofficial@gmail.com>
2024-12-27 11:44:49 +0200

    Update Google login redirection URL and improve error message handling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -28,11 +28,14 @@ const generateRefreshToken = async (userId) => {
 
 export const googleLogin = async (req, res) => {
     try {
-        account.createOAuth2Session('google');
+        const redirectUrl = 'https://backend-ewth-production.up.railway.app/api/auth/google-callback';
+        const googleAuthUrl = `${process.env.APPWRITE_ENDPOINT}/v1/account/sessions/oauth2/google?project=${process.env.APPWRITE_PROJECT_ID}&success=${redirectUrl}&failure=${redirectUrl}`;
+        res.redirect(googleAuthUrl);
     } catch (error) {
-        res.status(500).json({ message: 'Google Login Failed', error });
+        res.status(500).json({ message: 'Google Login failed', error });
     }
-}
+};
+
 
 export const googleCallback = async (req, res) => {
     try {
