f7b6c32cbd42e8ea2f2fdb91c49c9eab6943da90
elizadoltu <elizadoltuofficial@gmail.com>
2024-12-27 12:45:16 +0200

    Add localhost to CORS origin for local development

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ userGameProgressTable();
 createMetadataTables();
 
 app.use(cors({
-    origin: 'https://everything-with-the-unknown-app.net',
+    origin: ['https://everything-with-the-unknown-app.net', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }));
