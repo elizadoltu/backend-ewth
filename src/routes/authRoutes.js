d0243ba38ca53185e17b1ac7dfb9d09aac973c08
elizadoltu <elizadoltuofficial@gmail.com>
2024-12-28 12:21:36 +0200

    Fix typo in email verification route handler name in authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-import { login, logout, register, googleLogin, googleRegister, verifiyEmail } from '../controllers/authController.js';
+import { login, logout, register, googleLogin, googleRegister, verifyEmail } from '../controllers/authController.js';
 import roleMiddleware from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
@@ -11,7 +11,7 @@ router.post('/register', register);
 router.get('/admin', [authMiddleware, roleMiddleware(['admin'])], (req, res) => {
     res.json({ message: 'Welcome, admin!' });
 });
-router.post('/verify-email', verifiyEmail);
+router.post('/verify-email', verifyEmail);
 
 router.get('/google/login', googleLogin);
 router.get('/google/register', googleRegister);
